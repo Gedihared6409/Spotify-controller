9635fbbfddb0cde3630d28f685ecab107f066b32
Ali <gediali019@gamil.com>
2022-12-05 20:01:32 +0300

    removed django heroku

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,4 +132,3 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-django_heroku.settings(locals())
