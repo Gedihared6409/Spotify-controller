bcde92269bd60881ba394a41d015a133f168b82e
Gedihared6409 <gediali019@gmail.com>
2023-06-27 04:55:54 +0900

    added redirect uri

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,4 +1,4 @@
 
-CLIENT_ID = "f12d2ae0c6984707b163b4f8ea863185"
-CLIENT_SECRET = "e6c83ff0779a4692b98686b223cef49c"
+CLIENT_ID = "150b98f9f6064e708603f5ae6e45fee6"
+CLIENT_SECRET = "5b90433f64c542f0af6025d0cd857828"
 REDIRECT_URI = "https://musiccontroller-6973c97f0a15.herokuapp.com/spotify/redirect"
